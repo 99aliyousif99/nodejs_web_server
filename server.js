9d5d55b4ba19af07476fdd579ec438c743e0bd70
99aliyousif99 <128894509+99aliyousif99@users.noreply.github.com>
2024-07-11 15:07:19 +0300

    commit

--- server.js
+++ server.js
@@ -1,42 +1,42 @@
-const path = require("path");
-const {logger} = require("./middleware/logEvents");
-const express = require("express");
-const cors = require("cors");
+const express = require('express');
 const app = express();
-const PORT = process.env.PORT || 3000;
+const path = require('path');
+const cors = require('cors');
+const corsOptions = require('./config/corsOptions');
+const { logger } = require('./middleware/logEvents');
+const errorHandler = require('./middleware/errorHandler');
+const PORT = process.env.PORT || 3500;
 
+// custom middleware logger
 app.use(logger);
 
+// Cross Origin Resource Sharing
+app.use(cors(corsOptions));
 
-app.use(cors());
-
+// built-in middleware to handle urlencoded form data
 app.use(express.urlencoded({ extended: false }));
 
+// built-in middleware for json 
 app.use(express.json());
 
-app.use(express.static(path.join(__dirname, "public")));
+//serve static files
+app.use('/', express.static(path.join(__dirname, '/public')));
 
-app.get("/", (req, res) => {
-  //res.sendFile('./views/index.html', { root: __dirname });
-  res.sendFile(path.join(__dirname, "views", "index.html"));
+// routes
+app.use('/', require('./routes/root'));
+app.use('/employees', require('./routes/api/employees'));
+
+app.all('*', (req, res) => {
+    res.status(404);
+    if (req.accepts('html')) {
+        res.sendFile(path.join(__dirname, 'views', '404.html'));
+    } else if (req.accepts('json')) {
+        res.json({ "error": "404 Not Found" });
+    } else {
+        res.type('txt').send("404 Not Found");
+    }
 });
-app.get("/new-page(.html)?", (req, res) => {
-  
-  res.sendFile(path.join(__dirname, "views", "new-page.html"));
-});
 
-app.get("/old-page(.html)?", (req, res) => {
-  
-  res.redirect(301,"/new-page.html");
-});
+app.use(errorHandler);
 
-
-app.get('/hello(.html)?', (req, res,next) => {
-  console.log("tried to access /hello(.html)?");
-  next();
-}, (req, res) => (res.send("Hello, World!")));
-
-
-app.get('/*', (req, res) => {
-  res.status(404).sendFile(path.join(__dirname, "views", "404.html"));});
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
